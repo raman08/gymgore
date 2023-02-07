9ee1411ac45283368e33e883731d2b23d7d3de5a
Raman Preet Singh <ramanpre0810@gmail.com>
2023-02-07 18:11:13 +0530

    Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import { SessionProvider } from "next-auth/react";
 export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
+}: {
+	Component: any;
+	pageProps: { session: any };
 }) {
 	return (
 		<SessionProvider session={session}>
